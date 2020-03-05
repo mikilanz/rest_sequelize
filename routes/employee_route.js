e1e110bf7e8359b97e7afb4a10d190ac2d8ecf86
mikilanz <imanueljoshua@ymail.com>
2020-03-06 01:29:52 +0700

    add put employee

--- routes/employee_route.js
+++ routes/employee_route.js
@@ -27,6 +27,34 @@ module.exports = function(router) {
     }
   });
 
+  router.get("/employee/:id", async function (req, res) {
+    try {
+      const employees = await employee.findAll({
+        where: { id: req.params.id }
+      });
+      if (employees.length !== 0) {
+        res.json({
+          'status': '200',
+          'messages': 'Success',
+          'data': employees
+        })
+      } else {
+        res.json({
+          'status': '400',
+          'messages': 'EMPTY',
+          'data': {}
+        })
+      }
+    } catch (err) {
+      res.json({
+        'status': '400',
+        'messages': err.messages,
+        'data': {}
+      })
+    }
+  });
+
+
   router.post('/employee', async function (req, res, next) {
     try {
       const {
@@ -46,7 +74,7 @@ module.exports = function(router) {
     if (employees) {
       res.status(201).json({
         'status': 'OK',
-        'messages': 'User berhasil ditambahkan',
+        'messages': 'Data employee berhasil ditambahkan',
         'data': employees,
       })
     }
@@ -59,25 +87,50 @@ module.exports = function(router) {
    }
   });
 
-  // router.get("/employee/:id", (req, res) => {
-  //   employee.findAll({
-  //     where: { id: req.params.id }
-  //   })
-  //     .then(physician => {
-  //       res.json(physician[0]);
-  //     })
-  //     .catch(err => res.json(err));
-  // });
+  router.put('/employee/:id', async function (req, res, next) {
+    try {
+      const {
+        nama_employee,
+        nip,
+        tanggal_join
+      } = req.body;
 
-  // router.post("/employee", (req, res) => {
-  //   employee.create({
-  //     name: req.body.name
-  //   })
-  //     .then(res => {
-  //       res.json(res);
-  //     })
-  //     .catch(err => res.json(err));
-  // });
+      const employees = await employee.update({
+        nama_employee: nama_employee,
+        nip: nip,
+        tanggal_join: tanggal_join
+      },{ where: { id: req.params.id } });
+    if (employees != 0) {
+      res.status(201).json({
+        'status': 'OK',
+        'messages': 'Data Employee berhasil diupdate',
+        'data': employees,
+      })
+    } else{
+      res.status(404).json({
+        'status': '404',
+        'messages': 'ID not found',
+        'data': employees,
+      })
+    }
+   } catch (err) {
+     res.status(400).json({
+       'status': 'ERROR',
+       'messages': err.message,
+       'data': {},
+     })
+   }
+  });
+  router.put('/employee', async function (req, res, next) {
+    
+     res.status(405).json({
+       'status': '405 ',
+       'messages': 'method not allowed. please use param id',
+       'data': {},
+     })
+   
+  });
+
 
   // router.put("/employee/:id", (req, res) => {
   //   employee.update({ name: req.body.name }, { where: { id: req.params.id } })
